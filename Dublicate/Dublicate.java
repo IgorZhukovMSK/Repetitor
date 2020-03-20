120ea3b746c4e7a2415f18c963bfd93154d765b7
IgorZhukovMSK <vot_tak78@mail.ru>
2020-03-20 20:06:05 +0300

    исправление

--- Dublicate/Dublicate.java
+++ Dublicate/Dublicate.java
@@ -1,6 +1,5 @@
 package Dublicate;
 
-<<<<<<< HEAD
 /*
 Напишите метод, который на вход получает коллекцию объектов, а возвращает коллекцию уже без дубликатов.
  */
@@ -36,38 +35,3 @@ public class Dublicate {
       }
 }
 
-=======
-import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.ArrayList;
-
-public class Dublicate {
-    public Dublicate() {
-    }
-
-    public static void main(String[] args) throws IOException {
-        ArrayList array = new ArrayList();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int count;
-        for(count = 0; count < 5; ++count) {
-            array.add(br.readLine());
-        }
-
-        count = 0;
-        String str = null;
-
-        for(int i = 0; i < array.size(); ++i) {
-            for(int j = i + 1; j < array.size(); ++j) {
-                if (array.get(i).equals(array.get(j))) {
-                    ++count;
-                    str = String.valueOf(array.get(i));
-                }
-            }
-        }
-
-        System.out.println(count + " " + str);
-    }
-}
->>>>>>> 619e33d755aac079fb9b880203b8f872793d4a32
--- Dublicate/Main.java
+++ Dublicate/Main.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
@@ -20,7 +22,12 @@ public class Main {
                   }
             }
 
-            System.out.println(new Dublicate().Dublicate(array));
+
+            Set set = new HashSet();
+            set.addAll(array);
+
+             System.out.println(set);
+
 
 
       }
