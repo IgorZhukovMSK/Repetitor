db37beb4414bfdd2097f41151cfcf6ddfaa8fee7
IgorZhukovMSK <vot_tak78@mail.ru>
2020-03-23 20:06:36 +0300

    версия с HashMap

--- Str/Main.java
+++ Str/Main.java
@@ -4,6 +4,9 @@ package Str;
     Задание:
      Вам дана произвольная строка. Создать метод, который для любого слова будет выводить частоту его встречаемости в этой строке.
 
+         2 ую версию программы, которая для решения будет использовать коллекцию HashMap.
+         Тогда вам потребуется сначала заполнить ее парами ключ-значение, а затем создать метод,
+         который в качестве параметров будет получать HashMap и искомое слово и возвращать частоту встречаемости искомого слова.
 
 
     текст для проверки
@@ -14,15 +17,12 @@ package Str;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-
-        //static void printCountWord () {
 
         BufferedReader brText = new BufferedReader(new InputStreamReader(System.in));
         String strText = brText.readLine();
@@ -30,28 +30,32 @@ public class Main {
         String strWord = brWord.readLine();
 
 
-        List<String> arrText = new ArrayList<>();
-
         String tempText = strText.replaceAll("(?u)[^\\pL ]", "").toLowerCase();
 
         String[] parts = tempText.split(" ");
 
+        HashMap<String, Integer> hashMapStr = new HashMap<>();
+
+
         for (int i = 0; i < parts.length; i++) {
+            int count = 0;
+            for (int j = 0; j < parts.length; j++) {
 
-            arrText.add(parts[i]);
-        }
-
-        int count = 0;
-
-        for (String text : arrText) {
-
-            if (strWord.toLowerCase().equals(text)) {
-                count++;
-
+                if (parts[i].equals(parts[j])) {
+                    count++;
+                    hashMapStr.put(parts[i], count);
+                }
             }
         }
 
-        System.out.print("Слово " + strWord + " встречается " + count + " раз");
+        
+        for (Map.Entry<String, Integer> pair : hashMapStr.entrySet()) {
+
+        if (strWord.equals(pair.getKey()))
+
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+//        System.out.print("Слово " +  " встречается " + count + " раз"); //strWord +
     }
 }
 
